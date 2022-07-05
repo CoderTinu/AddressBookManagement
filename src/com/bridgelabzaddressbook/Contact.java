9264211038c1fa9095c662c9d321550f78503b08
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-07-05 13:02:00 +0530

    Ability to add new conatact

--- src/com/bridgelabzaddressbook/Contact.java
+++ src/com/bridgelabzaddressbook/Contact.java
@@ -11,5 +11,26 @@ public class Contact {
 	private long phone;
 	private String email;
 	
+	public Contact(String fname,String lname,String address,String city,String state,int zip,long phone,String email) {
+		this.firstname=fname;
+		this.lastname=lname;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phone=phone;
+		this.email=email;
+	}
+	
+	@Override
+	
+	public String toString()
+	{
+		return "First name is:"+firstname+"\nLastName is: "+lastname+"\nAddress is: "+address+"\nCity is: "+city+"\nZip is:"+zip+"\n State is:"+state+"\nPhone Number is:"
+	+phone+"\nEmail is: "+email;
+				 
+	}
+
+	
 
 }
