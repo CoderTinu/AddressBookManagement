c4bf1a623f606e89456da86abe594bb417739de7
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-07-05 12:43:06 +0530

    Added: Welcome to addressBook

---
+++ src/com/bridgelabzaddressbook/AddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabzaddressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+	
+		
+		
+		public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program ");
+		
+		
+	}
+}
