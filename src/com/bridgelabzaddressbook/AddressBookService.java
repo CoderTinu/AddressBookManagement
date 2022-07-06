f90f301ee7ad68e100d88a01c8e6288ca9bb7d32
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-07-06 17:51:36 +0530

    Ability to add multiple person

--- src/com/bridgelabzaddressbook/AddressBookService.java
+++ src/com/bridgelabzaddressbook/AddressBookService.java
@@ -1,102 +1,134 @@
 package com.bridgelabzaddressbook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
+
+
 
 public class AddressBookService {
 	public static Scanner sc = new Scanner(System.in);
-	public static ArrayList<Contact> arrayOfContacts = new ArrayList<Contact>();
-	public static void addContact()
-	{
-		System.out.println("Please Enter your first name:");
-		String firstname =sc.next();
-		System.out.println("Please Enter your last name:");
-		String lastname=sc.next();
-		System.out.println("Please enter your address:");
-		String address = sc.next();
-		System.out.println("Plese Enter city:");
-		String city = sc.next();
-		System.out.println("Please Enter State: ");
-		String state = sc.next();
-		System.out.println("Please enter zip code: ");
-		int zip = sc.nextInt();
-		System.out.println("Please enter phone number");
-		long phone =sc.nextLong();
-		System.out.println("Please enter email id: ");
-		String email = sc.next();
-		
-		Contact c= new Contact(firstname,lastname,address,city,state,zip,phone,email);
-		arrayOfContacts.add(c);
-	}
-	
-	public static void display()
-	{
-		for (Contact c:arrayOfContacts) {
-			System.out.println(c);
-		}
-	}
-	
-	private static int edit(String editName)
-	{
-		for (Contact contact : arrayOfContacts) {
-			if(editName.compareToIgnoreCase(contact.getFirstName())==0) {
-				return arrayOfContacts.indexOf(contact);
-			}
-			
-		}
-		
-		return -1;
-	}
-	/*public static void editContact()
-	{
-		System.out.println("Enter the name you want to edit");
-		String editName=sc.next();
-		int ans = edit(editName);
-		if(ans== -1)
-		{
-			System.out.println("Contact with name"+editName+" not found");
-			
-		}
-		else
-		{
-			System.out.println("Found the contact\nplease edit the details:");
-			addContact();
-		}
-	}*/
-	
-	private static int deleteContact(String deleteName)
-	{
-		for (Contact contact: arrayOfContacts) {
-			if(deleteName.compareToIgnoreCase(contact.getFirstName())==0) {
-				return arrayOfContacts.indexOf(contact);
-			}
-			}
-		return -1;
-		
-		}
-	
-	public static void deleteContact()
-	{
-		System.out.println("Enter name to delete");
-		String deleteName=sc.next();
-		int ans=deleteContact(deleteName);
-		if(ans==-1)
-		{
-			System.out.println("Contact with name"+deleteName+"not found");
-		}
-		else {
-			System.out.println("Delete name :"+deleteName+"have been deleted");
-			arrayOfContacts.clear();
-			if(arrayOfContacts.isEmpty())
-			{
-				System.out.println("No new contacts");
-			}
-			else {
-				for(Contact c:arrayOfContacts) {
-					System.out.println(c);
-				}
-			}
-		}
-		
-	}
+    public static ArrayList<Contacts> arrayOfContacts;
+    public static HashMap<String, ArrayList<Contacts>> hashMapOfAddressBooks = new HashMap<>();
+
+    public static ArrayList<Contacts> findAddressBook(String name) {
+        for (Entry<String, ArrayList<Contacts>> iterator : hashMapOfAddressBooks.entrySet()) {
+            if (iterator.getKey().equals(name)) {
+                return iterator.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static String addAddressBook() {
+        System.out.println("Enter the Address Book");
+        String addressBookName = sc.next();
+        if (findAddressBook(addressBookName) != null) {
+            System.out.println("This Address Book already exists");
+            System.out.println(hashMapOfAddressBooks.get(addressBookName));
+            return null;
+        }
+        return addressBookName;
+    }
+    public static int findContact(ArrayList<Contacts> arrayList)
+    {
+        System.out.println("Enter the name: ");
+        String editName = sc.next();
+        for(Contacts c : arrayList)
+        {
+            if(editName.compareToIgnoreCase(c.getFirstName()) == 0)
+            {
+                return arrayList.indexOf(c);
+            }
+        }
+        return -1;
+    }
+    public static void addContact(String bookName) {
+        System.out.println("Please enter your first name :");
+        String first_name = sc.next();
+        System.out.println("Please enter your last name :");
+        String last_name = sc.next();
+        sc.nextLine();
+        System.out.println("Please enter your address :");
+        String address = sc.nextLine();
+        System.out.println("Please enter your city :");
+        String city = sc.next();
+        System.out.println("Please enter your state :");
+        String state = sc.next();
+        System.out.println("Please enter your zip code :");
+        int zip = sc.nextInt();
+        System.out.println("Please enter your phone number :");
+        long phone = sc.nextLong();
+        System.out.println("Please enter your email id :");
+        String email_id = sc.next();
+        Contacts c = new Contacts(first_name, last_name, address, city, state, zip, phone, email_id);
+        if(findAddressBook(bookName) != null)
+        {
+            hashMapOfAddressBooks.get(bookName).add(c);
+            return;
+        }
+        arrayOfContacts =  new ArrayList<Contacts>();
+        arrayOfContacts.add(c);
+        hashMapOfAddressBooks.put(bookName,arrayOfContacts);
+
+    }
+
+    public static void display() {
+        System.out.println(" Please enter the name of the address book: ");
+        String name = sc.next();
+        if(hashMapOfAddressBooks.get(name).isEmpty())
+        {
+            System.out.println("The Address Book is empty.");
+            return;
+        }
+        System.out.println(hashMapOfAddressBooks.get(name));
+    }
+    public static void editContact() {
+        System.out.println("Enter the Address book you want to edit.");
+        String addressBookEdit = sc.next();
+        ArrayList<Contacts> arrayList = findAddressBook(addressBookEdit);
+        if(arrayList==null)
+        {
+            System.out.println("Address book not found");
+            return;
+        }
+        if (arrayList.isEmpty()) {
+            System.out.println("Address book with name " + addressBookEdit + " is empty");
+            return;
+        }
+       int index =  findContact(arrayList);
+        if(index == -1)
+        {
+            System.out.println("Details with this name is not found");
+            return;
+        }
+        System.out.println("Found the Contact");
+        arrayList.remove(index);
+        addContact(addressBookEdit);
+
+    }
+
+    public static void deleteContact() {
+        System.out.println("Enter the Address book you want to delete.");
+        String addressBookDel = sc.next();
+        ArrayList<Contacts> arrayList = findAddressBook(addressBookDel);
+        if(arrayList==null)
+        {
+            System.out.println("Address book not found");
+            return;
+        }
+        if (arrayList.isEmpty()) {
+            System.out.println("Address book with name " + addressBookDel + " is empty");
+            return;
+        }
+        int index =  findContact(arrayList);
+        if(index == -1)
+        {
+            System.out.println("Details with this name is not found");
+            return;
+        }
+        System.out.println("Deleted the Contact");
+        arrayList.remove(index);
+    }
 }
---
+++ src/com/bridgelabzaddressbook/Contacts.java
@@ -0,0 +1,36 @@
+package com.bridgelabzaddressbook;
+
+public class Contacts {
+	
+	private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phone;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Contacts(String first_name, String last_name, String address, String city, String state, int zip, long phone, String email_id) {
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email_id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
+                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email+"\n";
+    }
+	
+}
