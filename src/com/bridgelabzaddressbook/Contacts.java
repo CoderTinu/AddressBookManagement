e81dd23ae160fcf5d94d67d47a714f312a7a5ff1
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-07-06 18:05:10 +0530

    Search person by city or state

--- src/com/bridgelabzaddressbook/Contacts.java
+++ src/com/bridgelabzaddressbook/Contacts.java
@@ -5,6 +5,19 @@ public class Contacts {
 	private String firstName;
     private String lastName;
     private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     private String city;
     private String state;
     private int zip;
